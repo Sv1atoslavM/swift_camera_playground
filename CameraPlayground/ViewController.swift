7c73c84530e38c8d57908bf01ce3510c7f62659b
Sv1atoslavM <sviatoslav.m@radency.com>
2022-07-08 16:49:57 +0300

    Backup

--- CameraPlayground/ViewController.swift
+++ CameraPlayground/ViewController.swift
@@ -27,11 +27,15 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
     
     
     override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
         captureSession.startRunning()
     }
     
     override func viewDidDisappear(_ animated: Bool) {
         captureSession.stopRunning()
+        
+        super.viewDidDisappear(animated)
     }
     
     override func viewDidLoad() {
@@ -83,22 +87,19 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
         previewLayer.videoGravity = .resizeAspectFill
         previewView.layer.addSublayer(previewLayer)
         
-        let bounds = rootView.layer.bounds
-        
         detectionOverlay = CALayer() // container layer that has all the renderings of the observations
         detectionOverlay.name = "DetectionOverlay"
         detectionOverlay.bounds = CGRect(origin: .zero, size: bufferSize)
-        detectionOverlay.position = CGPoint(x: bounds.midX, y: bounds.midY)
         previewView.layer.addSublayer(detectionOverlay)
     }
     
     override func viewDidLayoutSubviews() {
         
-        previewLayer?.frame = rootView.frame
+        let interfaceOrientation = rootView.window?.windowScene?.interfaceOrientation
         
-        if let connection = previewLayer?.connection, connection.isVideoOrientationSupported {
-            
-            let interfaceOrientation = rootView.window?.windowScene?.interfaceOrientation
+        previewLayer.frame = rootView.frame
+        
+        if let connection = previewLayer.connection, connection.isVideoOrientationSupported {
             
             switch interfaceOrientation {
             case .portraitUpsideDown:
@@ -135,46 +136,18 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
             
             DispatchQueue.main.async {
                 
-                self.detectionOverlay?.sublayers = nil // remove all the old recognized objects
-                                
                 CATransaction.begin()
                 CATransaction.setValue(kCFBooleanTrue, forKey: kCATransactionDisableActions)
                 
+                self.detectionOverlay?.sublayers = nil // remove all the old recognized objects
+                
                 for poseObservation in results {
                     guard let recognizedPoints = try? poseObservation.recognizedPoints(.all) else { return }
-                        
-                    let jointNames: [VNHumanBodyPoseObservation.JointName] = [
-                        // Head
-                        .leftEar,
-                        .rightEar,
-                        .leftEye,
-                        .rightEye,
-                        .neck,
-                        .nose,
-                        // Arms
-                        .leftShoulder,
-                        .rightShoulder,
-                        .leftElbow,
-                        .rightElbow,
-                        .leftWrist,
-                        .rightWrist,
-                        // Waist
-                        .root,
-                        // Legs
-                        .leftHip,
-                        .rightHip,
-                        .leftKnee,
-                        .rightKnee,
-                        .leftAnkle,
-                        .rightAnkle,
-                    ]
                     
                     // Retrieve the CGPoints containing the normalized X and Y coordinates.
-                    let imagePoints: [CGPoint] = jointNames.compactMap {
-                        guard let point = recognizedPoints[$0], point.confidence > 0 else { return nil }
-                            
+                    let imagePoints: [CGPoint] = recognizedPoints.values.compactMap {
                         // Translate the point from normalized-coordinates to image coordinates.
-                        return VNImagePointForNormalizedPoint(point.location, Int(self.bufferSize.width), Int(self.bufferSize.height))
+                        $0.confidence > 0 ? VNImagePointForNormalizedPoint($0.location, Int(self.bufferSize.width), Int(self.bufferSize.height)) : nil
                     }
                     
                     // Draw the points onscreen.
@@ -225,11 +198,11 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
     
     func updateLayerGeometry() {
         let bounds = rootView.layer.bounds
-        let xScale: CGFloat = bounds.size.width / bufferSize.height
-        let yScale: CGFloat = bounds.size.height / bufferSize.width
+        let xScale: CGFloat = bounds.width / bufferSize.height
+        let yScale: CGFloat = bounds.height / bufferSize.width
         
         var scale = fmax(xScale, yScale)
-        
+
         if scale.isInfinite { scale = 1.0 }
         
         CATransaction.begin()
